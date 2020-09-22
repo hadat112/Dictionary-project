68568ceb694231e2db6545fa7f667d17f65002b6
hadat1122001 <hadat1122001@gmail.com>
2020-09-22 23:48:14 +0700

    third commit.

--- src/DictionaryCommandline.java
+++ src/DictionaryCommandline.java
@@ -1,12 +1,22 @@
 public class DictionaryCommandline {
-    public void showAllWords(DictionaryManagement d){
+    DictionaryManagement d = new DictionaryManagement();
+    public void showAllWords(){
         for(int i = 0; i < d.dictionary.words.size(); i++){
             System.out.println(d.dictionary.words.get(i).getWord_target()
                     + " " + d.dictionary.words.get(i).getWord_explain());
         }
     }
 
-    public void dictionaryBasic(Object insertFromCommandline){
-         insertFromCommandline
+    public void dictionaryBasic(Word word){
+        d.insertFromCommandline(word);
+        showAllWords();
+    }
+
+    public static void main(String[] args) {
+        Word word = new Word("hadat", "depzaivcl");
+        Word word1 = new Word("hung", "occho");
+        DictionaryCommandline d = new DictionaryCommandline();
+        d.dictionaryBasic(word);
+        d.dictionaryBasic(word1);
     }
 }
